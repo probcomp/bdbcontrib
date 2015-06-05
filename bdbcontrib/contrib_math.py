976decb81e1e4e1bbb9302afe530396f30542404
Baxter Eaves <baxyjr@gmail.com>
2015-06-05 10:45:19 -0400

    BUGFIX: Added extra line to register register_bql_math doctring. Having a docstring w/ only one line causes .help to crash.

--- bdbcontrib/contrib_math.py
+++ bdbcontrib/contrib_math.py
@@ -5,7 +5,9 @@ import math
 
 @bayesdb_shell_cmd('register_bql_math_functions')
 def register_bql_math(self, args):
-    '''Adds basic math functions to BQL'''
+    '''Adds basic math functions to BQL
+    (No arguments)
+    '''
     self._bdb.sqlite3.create_function('exp', 1, math.exp)
     self._bdb.sqlite3.create_function('sqrt', 1, math.sqrt)
     self._bdb.sqlite3.create_function('pow', 2, pow)
