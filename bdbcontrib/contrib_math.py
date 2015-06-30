5f10e48211e91e77c9329c435d9ad8b091e06845
Baxter Eaves <baxyjr@gmail.com>
2015-06-30 09:46:33 -0400

    added: LINFOOT information BQL function

--- bdbcontrib/contrib_math.py
+++ bdbcontrib/contrib_math.py
@@ -9,3 +9,5 @@ def register_bql_math(self):
     self._bdb.sqlite3.create_function('sqrt', 1, math.sqrt)
     self._bdb.sqlite3.create_function('pow', 2, pow)
     self._bdb.sqlite3.create_function('log', 1, math.log)
+    self._bdb.sqlite3.create_function('linfoot', 1,
+                                      lambda x: math.sqrt(1.-math.exp(-2.*x)))
