5cf385247f8e6e57c531d285390f45ae4f9c076e
F Saad <fsaad@mit.edu>
2015-08-24 20:44:45 -0400

    Initial commit for estimate ll.

--- bdbcontrib/contrib_exp.py
+++ bdbcontrib/contrib_exp.py
@@ -52,3 +52,79 @@ def run_experiment(self, argin):
         return
     except SystemExit:
         return  # This happens when --help is invoked.
+
+@bayesdb_shell_cmd('est_ll')
+def estimate_log_likelihood(self, argin):
+    '''
+    Estimate the log likelihood of a dataset
+    USAGE: .est_ll <generator> <table> <targets_cols ...> <given_cols ...>
+
+    Examples:
+    bayeslite> .est_ll my_gen my_table --target-cols age height weight --givens nationality=USA gender=Male
+    '''
+    parser = ArgumentParser(prog='.est_ll')
+    parser.add_argument('generator', type=str,
+        help='Name of the generator.')
+    parser.add_argument('table', type=str,  help='Name of the table.')
+    parser.add_argument('--target-cols', nargs='*',
+        help='Sequence of target columns to evaluate the log likelhood. '
+        'By default, all columns in <table> will be used.')
+    parser.add_argument('--givens', nargs='*',
+        help='Sequence of columns and observed values to condition on. '
+        'The required format is [<col> <val>...] .')
+
+    try:
+        cargs = parser.parse_args(shlex.split(argin))
+    except ArgparseError as e:
+        self.stdout.write('%s' % (e.message,))
+        return
+
+    # If no target columns specified, use all.
+    if cargs.target_cols:
+        target_cols = cargs.target_cols
+    else:
+        generator_id = core.bayesdb_get_generator(self._bdb, cargs.generator)
+        target_cols = core.bayesdb_generator_column_names(self._bdb,
+            generator_id)
+    target_cols = [sqlite3_quote_name(col) for col in target_cols]
+
+    # If no givens columns, assume no conditions.
+    if cargs.givens:
+        import ipdb; ipdb.set_trace()
+        cols = [sqlite3_quote_name(col) for col in cargs.givens[::2]]
+        vals = [float(val) for val in cargs.givens[1::2]]
+        assert len(cols) == len(vals)
+        givens = zip(cols,vals)
+    else:
+        givens = []
+
+    # Obtain the dataset table
+    sql = '''
+        SELECT {} FROM {};
+    '''.format(','.join(target_cols), sqlite3_quote_name(cargs.table))
+    dataset = self._bdb.execute(sql)
+
+    # Compute the log-likelihood of the target_cols, subject to givens.
+    # XXX This code is wrong due to shortcomings in BQL:
+    #  - BQL cannot evaluate joint density. Assume that all the rows are IID,
+    #  and that all the columns factor into their marginal density.
+    #  - BQL cannot evaluate conditional density. This query will not compile.
+    LL = 0
+    for row in dataset:
+        # XXX Wrong: assume joint factors into product of marginals.
+        for col, val in zip(target_cols, row):
+            # XXX Wrong: Code will crash, no conditional densities
+            if givens:
+                bql = '''
+                ESTIMATE PROBABILITY OF {}=? FROM {} GIVEN {} LIMIT 1
+                '''.format(col, sqlite3_quote_name(cargs.generator),
+                    ','.join(['{}={}'.format(c,v) for (c,v) in givens]))
+            else:
+                bql = '''
+                ESTIMATE PROBABILITY OF {}=? FROM {} LIMIT 1
+                '''.format(col, sqlite3_quote_name(cargs.generator))
+
+            crs = self._bdb.execute(bql, (val,))
+            LL += math.log(crs.fetchall()[0][0])
+
+    return LL
