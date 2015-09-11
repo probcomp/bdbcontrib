fe62e09be91df25415cf74e6e40f1393d8338258
F Saad <fsaad@mit.edu>
2015-09-11 16:52:24 -0400

    Convert estiamte_ll to an api call.

--- hooks/contrib_diagnostics.py
+++ hooks/contrib_diagnostics.py
@@ -38,10 +38,9 @@ experiments = {
 
 
 @bayesdb_shell_cmd('experiment')
-def run_bdb_exp(self, argin):
-    """
-    Launch an experimental inference quality test (requires the bdbexp module).
-    USAGE: .experiment <exp_name> [exp_args ...]
+def run_bdb_experiment(self, argin):
+    """ Launch an experimental inference quality test (requires bdbexp module).
+    <exp_name> [exp_args ...]
 
     <exp_name>
         permute         univariate_kl
@@ -57,6 +56,8 @@ def run_bdb_exp(self, argin):
     bayeslite> .experiment predictive_pdf --help
     bayeslite> .experiment haystacks --n_iter=200 --n_distractors=4
     """
+    # TODO: Use ArgumentParser instead.
+    # TODO: Migrate this to the public API?
     expname = argin.split()[0] if argin != '' else argin
     if expname not in experiments:
         print 'Invalid experiment {}'.format(expname)
@@ -74,18 +75,18 @@ def run_bdb_exp(self, argin):
 
 @bayesdb_shell_cmd('est_ll')
 def estimate_log_likelihood(self, argin):
-    """
-    Estimate the log likelihood of a dataset using N samples.
-    USAGE: .est_ll <generator> <table> [<--targets-cols ...>] [<--given-cols ...>] [<--samples> N]
+    """Estimate the log likelihood of a dataset.
+    <table> <generator> [<--targets-cols ...>] [<--given-cols ...>] [<--samples> N]
 
     Examples:
-    bayeslite> .est_ll my_gen my_table --target-cols height weight --given-cols age nationality 17 --samples 1000
+    bayeslite> .est_ll people people_gen --targets height --givens age 1 nationality 17 --samples 1000
     """
     parser = ArgumentParser(prog='.est_ll')
+    parser.add_argument('table', type=str,
+        help='Name of the table.')
     parser.add_argument('generator', type=str,
         help='Name of the generator.')
-    parser.add_argument('table', type=str,  help='Name of the table.')
-    parser.add_argument('--target-cols', nargs='*',
+    parser.add_argument('--targets', nargs='*',
         help='Sequence of target columns to evaluate the log likelhood. '
         'By default, all columns in <table> will be used.')
     parser.add_argument('--givens', nargs='*',
@@ -101,70 +102,11 @@ def estimate_log_likelihood(self, argin):
         self.stdout.write('%s' % (e.message,))
         return
 
-    cargs = args
+    ll = bdbcontrib.api.estimate_log_likelihood(self._bdb, args.table,
+        args.generator, targets=args.targets, givens=args.givens,
+        samples=args.samples)
 
-    # If no target columns specified, use all.
-    if args.target_cols:
-        target_cols = args.target_cols
-    else:
-        generator_id = core.bayesdb_get_generator(self._bdb, args.generator)
-        target_cols = core.bayesdb_generator_column_names(self._bdb,
-            generator_id)
-    target_cols = [sqlite3_quote_name(col) for col in target_cols]
-
-    # If no givens columns, assume no conditions.
-    if args.givens:
-        cols = [sqlite3_quote_name(col) for col in args.givens[::2]]
-        vals = [float(val) for val in args.givens[1::2]]
-        assert len(cols) == len(vals)
-        givens = zip(cols,vals)
-    else:
-        givens = []
-
-    # Obtain the dataset table.
-    table = sqlite3_quote_name(args.table.strip(';'))
-    sql = '''
-        SELECT {} FROM {};
-    '''.format(','.join(target_cols), table)
-    dataset = self._bdb.execute(sql)
-
-    # Obtain number of rows in the dataset.
-    samples = args.samples
-    n_rows = self._bdb.execute('''
-        SELECT COUNT(*) FROM {}
-        '''.format(table)).fetchall()[0][0]
-    if samples is None or n_rows < samples:
-        samples = n_rows
-
-    # Compute the log-likelihood of the target_cols, subject to givens.
-    # XXX This code is wrong due to shortcomings in BQL:
-    #  - BQL cannot evaluate joint density. Assume that all the rows are IID,
-    #  and that all the columns factor into their marginal density.
-    #  - BQL cannot evaluate conditional density. This query will not compile.
-    LL = 0
-    i = 0
-    for row in dataset:
-        if i > samples:
-            break
-        else:
-            i += 1
-        # XXX Wrong: assume joint factors into product of marginals.
-        for col, val in zip(target_cols, row):
-            if givens:
-                bql = '''
-                ESTIMATE PROBABILITY OF {}=? GIVEN ({}) FROM {} LIMIT 1
-                '''.format(col,
-                        ','.join(['{}={}'.format(c,v) for (c,v) in givens]),
-                        sqlite3_quote_name(args.generator))
-            else:
-                bql = '''
-                ESTIMATE PROBABILITY OF {}=? FROM {} LIMIT 1
-                '''.format(col, sqlite3_quote_name(args.generator))
-
-            crs = self._bdb.execute(bql, (val,))
-            LL += math.log(crs.fetchall()[0][0])
-
-    print LL
+    print ll
 
 # TODO Bring in estimate_kl_divergence from fsaad-kl-div branch in bayeslite.
 # @bayesdb_shell_cmd('est_kl')
