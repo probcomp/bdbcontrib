5005a205c7e3f75d6385a69a7eca9fd0b4985292
F Saad <fsaad@mit.edu>
2015-09-11 18:46:06 -0400

    Add copyright notices and organize imports.

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -1,14 +1,28 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright (c) 2010-2014, MIT Probabilistic Computing Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import copy
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from textwrap import wrap
 
 import bdbcontrib.bql_utils as bqlu
-
-from textwrap import wrap
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-import seaborn as sns
-import copy
-import pandas as pd
-import numpy as np
-
 
 MODEL_TO_TYPE_LOOKUP = {
     'normal_inverse_gamma': 'numerical',
