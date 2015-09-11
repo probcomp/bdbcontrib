65d5a456e0210a66cacabe2bc9a171a723ba9fa5
Alexey Radul <axch@mit.edu>
2015-09-11 15:48:00 -0400

    Record how we run tests in check.sh

---
+++ check.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -Ceu
+
+: ${PY_TEST:=`which py.test`}
+
+if [ ! -x "${PY_TEST}" ]; then
+    printf >&2 'unable to find pytest\n'
+    exit 1
+fi
+
+root=`cd -- "$(dirname -- "$0")" && pwd`
+
+(
+    set -Ceu
+    cd -- "${root}"
+    "$PY_TEST" "$@"
+)
