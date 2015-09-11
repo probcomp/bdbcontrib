235260832dc8b38811b20bf75350ad2fab4020c5
F Saad <fsaad@mit.edu>
2015-09-11 14:31:56 -0400

    Fix imports in facade.

--- src/facade.py
+++ src/facade.py
@@ -1,14 +1,30 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright (c) 2010-2014, MIT Probabilistic Computing Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 import bayeslite
 import bayeslite.crosscat
 import bayeslite.guess
 import bayeslite.read_pandas
-import bql_utils as bu
 
 from bayeslite.sqlite3_util import sqlite3_quote_name as sqlquote
-from bdbcontrib import draw_cc_state
 from crosscat.LocalEngine import LocalEngine
 from crosscat.MultiprocessingEngine import MultiprocessingEngine
+
+from bdbcontrib import bql_utils
+from bdbcontrib import crosscat_utils
 
 
 class BQLQueryResult(object):
@@ -36,7 +52,7 @@ class BQLQueryResult(object):
 
         if self._df is None:
             with self._bdb.savepoint():
-                self._df = bu.cursor_to_df(self._cursor)
+                self._df = bql_utils.cursor_to_df(self._cursor)
         return self._df
 
     def as_cursor(self):
@@ -159,6 +175,5 @@ class BayesDBClient(object):
         bdbcontrib.draw_cc_state.draw_state
         """
         with self.bdb.savepoint():
-            return draw_cc_state.draw_state(self.bdb, btable_name,
-                                            generator_name, modelno,
-                                            **kwargs)
+            return crosscat_utils.draw_state(self.bdb, btable_name,
+                generator_name, modelno, **kwargs)
