dc07421103cf2f93a917bdb651d16f82f12c992d
F Saad <fsaad@mit.edu>
2015-09-11 21:50:44 -0400

    Implement helper functions for future reuse.

--- src/diagnostic_utils.py
+++ src/diagnostic_utils.py
@@ -20,6 +20,60 @@ import bayeslite.core
 from bayeslite.sqlite3_util import sqlite3_quote_name
 
 
+def extract_target_cols(bdb, generator, targets=None):
+    """Extract target columns (helper for LL/KL query).
+
+    If targets is None, then a list of all sqlite3 quoted column names from
+    generator are returned.
+
+
+    Parameters
+    ----------
+    bdb : bayeslite.BayesDB
+        Active BayesDB instance.
+    generator : str
+        Name of generator.
+    targets : list<str>, optional
+        List of columns in the table.
+
+    Returns
+    -------
+    target_cols : list<str>
+    """
+    if targets is None:
+        generator_id = bayeslite.core.bayesdb_get_generator(bdb, generator)
+        targets = bayeslite.core.bayesdb_generator_column_names(bdb,
+            generator_id)
+    return map(sqlite3_quote_name, targets)
+
+
+def extract_given_cols_vals(givens=None):
+    """Extract target columns (helper for LL/KL).
+
+    If givens is None, then an empty list is returned. Otherwise an appropriate
+    list of tuples is returned, where the first element is the sqlite3 quoted
+    name and the second element is the constraint value.
+
+
+    Parameters
+    ----------
+    givens : list<str>, optional
+        List of columns in the table.
+
+    Returns
+    -------
+    given_cols_vals : list<tuple>
+    """
+    if givens is None:
+        return []
+
+    given_cols = map(sqlite3_quote_name, givens[::2])
+    given_vals = givens[1::2]
+    assert len(given_cols) == len(given_vals)
+
+    return zip(given_cols, given_vals)
+
+
 # TODO: Migrate from hooks/contrib_diagnostics. Need users run experiments?
 # def run_bdb_experiment(bdb, exp_args):
     # pass
@@ -29,7 +83,7 @@ from bayeslite.sqlite3_util import sqlite3_quote_name
 
 
 def estimate_log_likelihood(bdb, table, generator, targets=None, givens=None,
-        samples=None):
+        n_samples=None):
     """Estimate the log likelihood of a dataset.
 
     Parameters
@@ -46,6 +100,9 @@ def estimate_log_likelihood(bdb, table, generator, targets=None, givens=None,
     givens : list<tuple>, optional
         A list of [(column, value)] pairs on which to condition on. Defaults to
         no conditionals. See example for more details.
+    n_samples : int, optional
+        Number of rows from table to use in the computation. Defaults to all
+        the rows.
 
     Returns
     -------
@@ -59,22 +116,12 @@ def estimate_log_likelihood(bdb, table, generator, targets=None, givens=None,
         givens=[('nationality', 'USA'), ('age', 17)])
 
     """
-    # If no target columns specified, use all.
-    if targets is None:
-        generator_id = bayeslite.core.bayesdb_get_generator(bdb, generator)
-        targets = bayeslite.core.bayesdb_generator_column_names(bdb,
-            generator_id)
-    targets = map(sqlite3_quote_name, targets)
+    # Defaults to all columns if targets is None.
+    targets = extract_target_cols(bdb, generator, targets=targets)
 
-    # If no givens columns, assume no conditions.
-    if givens is None:
-        givens = []
-    else:
-        given_cols = map(sqlite3_quote_name, givens[::2])
-        given_vals = givens[1::2]
-        assert len(given_cols) == len(given_vals)
-        givens = zip(given_cols,given_vals)
-        givens = ','.join(['{}={}'.format(c,v) for (c,v) in givens])
+    # Defaults to no givens if givens is None
+    givens = extract_given_cols_vals(givens=givens)
+    givens = ','.join(['{}={}'.format(c,v) for (c,v) in givens])
 
     # Obtain the dataset table.
     table = sqlite3_quote_name(table.strip(';'))
@@ -84,12 +131,12 @@ def estimate_log_likelihood(bdb, table, generator, targets=None, givens=None,
     dataset = bdb.execute(sql)
 
     # Obtain number of rows in the dataset and samples to use.
-    samples = samples
+    n_samples = n_samples
     n_rows = bdb.execute('''
         SELECT COUNT(*) FROM {}
         '''.format(table)).fetchall()[0][0]
-    if samples is None or n_rows < samples:
-        samples = n_rows
+    if n_samples is None or n_rows < n_samples:
+        n_samples = n_rows
 
     # Compute the log-likelihood of the targets, subject to givens.
     # XXX This code is currently wrong due to shortcomings in BQL:
@@ -97,13 +144,14 @@ def estimate_log_likelihood(bdb, table, generator, targets=None, givens=None,
     #  and that all the columns factor into their marginal density.
     ll, i = 0, 0
     for row in dataset:
-        if i > samples:
+        if i > n_samples:
             break
         else:
             i += 1
         # XXX Wrong: assume joint factors into product of marginals.
         for col, val in zip(targets, row):
             if givens:
+                # XXX TODO write GIVEN in this query using bindings.
                 bql = '''
                 ESTIMATE PROBABILITY OF {}=? GIVEN ({}) FROM {} LIMIT 1
                 '''.format(col, givens, sqlite3_quote_name(generator))
@@ -116,3 +164,4 @@ def estimate_log_likelihood(bdb, table, generator, targets=None, givens=None,
             ll += math.log(crs.fetchall()[0][0])
 
     return ll
+
