8592b457788bf8f01240344730d51b25beeccd5c
F Saad <fsaad@mit.edu>
2015-09-16 15:06:32 -0400

    Remove dependence on bdbexp in hooks.

--- hooks/hook_diagnostics.py
+++ hooks/hook_diagnostics.py
@@ -16,60 +16,10 @@
 
 import shlex
 
-import bdbexp
 from bayeslite.shell.hook import bayesdb_shell_cmd
 
 import bdbcontrib
 from bdbcontrib.shell_utils import ArgparseError, ArgumentParser
-
-
-experiments = {
-    'haystacks' : bdbexp.haystacks,
-    'hyperparams' : bdbexp.hyperparams,
-    'infer' : bdbexp.infer,
-    'permute' : bdbexp.permute,
-    'recover' : bdbexp.recover,
-    'univariate_kl' : bdbexp.univariate_kldiv,
-    'univariate_pdf' : bdbexp.univariate_pdf
-    }
-
-
-@bayesdb_shell_cmd('experiment')
-def run_bdb_experiment(self, argin):
-    """launch experimental inference quality test
-    <exp_name> [exp_args ...]
-
-    (Requires bdbexp module.)
-
-    <exp_name>
-        permute         univariate_kl
-        haystacks       univariate_pdf
-        hyperparams     recover
-        infer
-
-    [exp_args]
-        To see experiment specific arguments, use
-        .experiment <exp_name> --help
-
-    Examples:
-    bayeslite> .experiment predictive_pdf --help
-    bayeslite> .experiment haystacks --n_iter=200 --n_distractors=4
-    """
-    # TODO: Use ArgumentParser instead.
-    # TODO: Migrate this to the public API?
-    expname = argin.split()[0] if argin != '' else argin
-    if expname not in experiments:
-        print 'Invalid experiment {}'.format(expname)
-        print 'For help use: .help experiment'
-        return
-
-    try:
-        experiments[argin.split()[0]].main(argin.split()[1:], halt=True)
-    except ArgparseError as e:
-        self.stdout.write('%s' % (e.message,))
-        return
-    except SystemExit:
-        return  # This happens when --help is invoked.
 
 
 @bayesdb_shell_cmd('est_ll')
