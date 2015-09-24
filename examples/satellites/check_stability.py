c85339d9042a696031553c46cc27191ba5ec5ae5
Alexey Radul <axch@mit.edu>
2015-09-24 12:22:45 -0400

    Draft a stability tester.
    Current state:
    - One query set (preeminence of US communications satellites in simulation results)
    - Distribution over saved bdb files
    - Distribution over number of models to estimate from
    - Aggregation across batches of independent models in the same saved file
    - Visualization pending

---
+++ examples/satellites/check_stability.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright (c) 2010-2014, MIT Probabilistic Computing Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import contextlib
+
+import bayeslite
+
+model_schedule = [1,2]
+model_skip = model_schedule[-1]
+n_replications = 2
+# TODO Expect the number of models in the file to be at least
+# model_skip * n_replications; excess models are wasted.
+
+def country_purpose_queries(bdb):
+    bdb.execute('''
+        CREATE TEMP TABLE satellite_purpose AS
+        SIMULATE country_of_operator, purpose FROM satellites_cc
+        GIVEN Class_of_orbit = 'GEO', Dry_mass_kg = 500
+        LIMIT 1000;
+    ''')
+    usa_ct_sql = '''
+        SELECT COUNT(*) FROM satellite_purpose
+        WHERE country_of_operator = 'USA'
+          AND purpose = 'Communications'
+    '''
+    (usa_ct,) = bdb.execute(usa_ct_sql).next()
+    hist_sql = '''
+        SELECT country_of_operator, purpose, COUNT(*) AS frequency
+        FROM satellite_purpose
+        GROUP BY country_of_operator, purpose
+        ORDER BY frequency DESC
+        LIMIT 2
+    '''
+    results = bdb.execute(hist_sql).fetchall()
+    usa_top = results[0][0] == 'USA' and results[0][1] == 'Communications'
+    by_much = results[0][2] > 2*results[1][2]
+    return [ ("USA-Communications count", usa_ct),
+             ("USA-Communications is top", usa_top),
+             ("USA-Communications is top by 2x", by_much),
+         ]
+
+def analyze_fileset(files):
+    """Aggregate all the queries over all the given bdb files.
+
+    Do seed and model count variation within each file by varying
+    model specs; assume the files represent analysis iteration
+    variation and report accordingly.
+
+    Alternative: could just assume the file name describes the file
+    opaquely.
+
+    """
+    # Keys are (file_name, model_ct, name); values are aggregated results
+    results = {}
+    for fname in files:
+        with bayeslite.bayesdb_open(fname) as bdb:
+            incorporate(results,
+                [((fname, model_ct, name), ress)
+                 for ((model_ct, name), ress) in analyze_queries(bdb)])
+
+def model_specs():
+    def spec_at(location, size):
+        """Return a 0-indexed model range
+        inclusive of lower bound and exclusive of upper bound."""
+        return (location * model_skip, location * model_skip + size)
+    return [spec_at(location, size)
+            for location in range(n_replications)
+            for size in model_schedule]
+
+@contextlib.contextmanager
+def model_restriction(bdb, gen_name, spec):
+    (low, high) = spec
+    model_count = 4 # TODO !
+    with bdb.savepoint_rollback():
+        bdb.execute('''drop models 0-%d from %s''' % (low-1, gen_name))
+        bdb.execute('''drop models %d-%d from %s'''
+                    % (high, model_count-1, gen_name))
+        yield
+
+def incorporate(running, new):
+    for (key, more) in new:
+        if key in running:
+            running[key] = merge_one(running[key], more)
+        else:
+            running[key] = more
+
+# These two functions are placeholders for type-dependent aggregation
+# of intermediate values (e.g., counting yes and no for booleans vs
+# accumulating lists of reals).
+def inc_singleton(val):
+    return [val]
+
+def merge_one(so_far, val):
+    so_far.extend(val)
+    return so_far
+
+def analyze_queries(bdb):
+    results = {} # Keys are (model_ct, name); values are aggregated results
+    for spec in model_specs():
+        (low, high) = spec
+        model_ct = high - low
+        with model_restriction(bdb, "satellites_cc", spec):
+            for queryset in [country_purpose_queries]:
+                incorporate(results, [((model_ct, name), inc_singleton(res))
+                                      for (name, res) in queryset(bdb)])
+    return results
+
+print analyze_fileset(["output/satellites-2015-09-24-axch-4m-%di.bdb" % i
+                       for i in range(4)])
