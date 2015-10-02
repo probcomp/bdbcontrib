fa8f07251224e34a0da50ca787e17c745fa421f4
F Saad <fsaad@mit.edu>
2015-10-02 12:34:28 -0400

    Add comments.

--- src/predictors/kepler_demo/plots.py
+++ src/predictors/kepler_demo/plots.py
@@ -2,13 +2,34 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import seaborn
 
 from matplotlib import rc
 
+try:
+    import seaborn
+except ImportError:
+    pass
+
+# Set up matplotlib.
+matplotlib.rcParams['font.weight'] = 'bold'
+matplotlib.rcParams['font.size'] = 24
+matplotlib.rcParams['lines.linewidth'] = 2
+matplotlib.rcParams['legend.fontsize'] = 16
+matplotlib.rcParams['axes.titlesize'] = 24
+matplotlib.rcParams['axes.labelsize'] = 20
+matplotlib.rcParams['axes.titleweight'] = 'bold'
+matplotlib.rcParams['axes.labelweight'] = 'bold'
+matplotlib.rcParams['axes.grid'] = True
+matplotlib.rcParams['xtick.labelsize'] = 16
+matplotlib.rcParams['ytick.labelsize'] = 16
+matplotlib.rcParams['legend.frameon'] = False
+
+# Global constants.
 GM = 398600.4418
 EARTH_RADIUS = 6378
+T = 1436
 
+# Helper functions.
 def compute_period(apogee_km, perigee_km):
     """Computes the period of the satellite in seconds given the apogee_km
     and perigee_km of the satellite.
@@ -25,21 +46,10 @@ def compute_T(a):
     T = 2 * np.pi * np.sqrt(a**3/GM) / 60.
     return T
 
-T = 1436
+# Read simulated data.
 KC = pd.read_csv('simulated/kc.csv')
 DC = pd.read_csv('simulated/dc.csv')
 EC = pd.read_csv('simulated/ec.csv')
-
-matplotlib.rcParams['font.weight'] = 'bold'
-matplotlib.rcParams['font.size'] = 24
-matplotlib.rcParams['lines.linewidth'] = 2
-matplotlib.rcParams['legend.fontsize'] = 16
-matplotlib.rcParams['axes.titlesize'] = 24
-matplotlib.rcParams['axes.labelsize'] = 20
-matplotlib.rcParams['axes.titleweight'] = 'bold'
-matplotlib.rcParams['axes.labelweight'] = 'bold'
-matplotlib.rcParams['xtick.labelsize'] = 16
-matplotlib.rcParams['ytick.labelsize'] = 16
 
 # Plot of the samples.
 fig, ax = plt.subplots()
@@ -63,14 +73,14 @@ ax.set_xlim([23000, 52000])
 ax.set_ylim([23000, 42000])
 ax.legend()
 
+
 # Plot for Implied Period and SMA.
 fig, (ax, ax2) = plt.subplots(nrows=1, ncols=2)
 fig.suptitle('Derived Orbital Statistics From Simulated '
     '(Apogee, Perigee) Given Period = 1436',
     )
 xs = range(len(KC))
-
-# PERIOD
+# -- Period.
 KC_period = compute_period(KC['Apogee_km'], KC['Perigee_km'])
 DC_period = compute_period(DC['Apogee_km'], DC['Perigee_km'])
 
@@ -82,8 +92,7 @@ ax.set_title('Implied Period')
 ax.set_xlabel('Simulation Trial')
 ax.set_xlim([min(xs), max(xs)])
 ax.legend(loc=3)
-
-# SMA
+# -- SMA.
 KC_sma = 0.5*(KC['Apogee_km']+KC['Perigee_km']) + EARTH_RADIUS
 DC_sma = 0.5*(DC['Apogee_km']+DC['Perigee_km']) + EARTH_RADIUS
 
@@ -95,6 +104,7 @@ ax2.hlines(compute_a(T), min(xs), max(xs), color='black',
 ax2.set_title('Implied Semi-Major Axis')
 ax2.set_xlabel('Simulation Trial')
 ax2.set_xlim([min(xs), max(xs)])
+
 
 # Overall Residual Plot.
 fig, ax = plt.subplots(nrows=1,ncols=2)
@@ -118,6 +128,7 @@ ax[1].set_xlabel('Residual Kilometers')
 ax[1].hist(DC_residA, bins=50, color='red', alpha=0.2, label='Default Model')
 ax[1].hist(KC_residA, bins=50, color='green', alpha=1., label='Orbital Model')
 ax[1].set_ylim(0, ax[1].get_ylim()[1]+2)
+
 
 # Apogee vs Period Simulations
 KJ = pd.read_csv('simulated/kj.csv')
