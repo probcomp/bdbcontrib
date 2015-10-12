c52e116ca1650fa083edde2bd9f3745d55abd961
Richard Tibbetts <tibbetts@innocuous.org>
2015-10-12 15:03:49 -0600

    Working angular data binding against the rest server.

--- examples/rest/static/rest-example.js
+++ examples/rest/static/rest-example.js
@@ -1,5 +1,12 @@
-jQuery(document).ready(function($) {
-    $("#tables-refresh").on("click", function() {
-        alert("Ha ha refresh");
+angular.module('bdbServices', ['ngResource'])
+.factory('Table', function($resource) {
+        return $resource('/api/v1/table/:tableName', {}, {
+            query: {method:'GET', params:{tableName:''}, isArray:true}
+        });
     });
-});
+
+
+angular.module('bdbRestExampleApp', ['ngResource', 'bdbServices'])
+    .controller('TablesListCtrl', function($scope, Table) {
+        $scope.tables = Table.query();
+    });
