d0dce813dc89c4ea1f3890331326de7c11154d3d
Alexey Radul <axch@mit.edu>
2015-10-26 11:51:29 -0400

    This desideratum is more robustly recorded as an issue.

--- src/predictors/multiple_regression.py
+++ src/predictors/multiple_regression.py
@@ -14,8 +14,6 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-# TODO: Update the interface for multivariate targets.
-
 import pickle
 
 import numpy as np
