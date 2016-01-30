d927b6051ebfda08c8721ce6db811a2391ada8d5
Gregory Marton <gremio@acm.org>
2016-01-29 19:40:00 -0500

    Add a logger that calls home the same way bayeslite sessions does. Add a context manager to make it easy to use. Leaving as a TODO to DRY it up with bayeslite/sessions.

--- src/loggers.py
+++ src/loggers.py
@@ -122,3 +122,66 @@ class IpyLogger(BqlLogger):
     IPython.utils.warn.info(msg_format % values)
   def warn(self, msg_format, *values):
     IPython.utils.warn.warn(msg_format % values)
+
+PROBCOMP_URL = 'https://projects.csail.mit.edu/probcomp/bayesdb/save_sessions.cgi'
+import requests
+import time
+import threading
+from version import __version__
+
+class CallHomeStatusLogger(BqlLogger):
+    # We will start a small thread for each call home so as to avoid
+    # network latency impacting notebook responsiveness.
+    def __init__(self, url=PROBCOMP_URL, post=None):
+        if post is None:
+            post = requests.post
+        self._post = post
+        self._url = url
+    def info(self, json_msg, *unused_values):
+        self._send(json_msg)
+    def warn(self, json_msg, *unused_values):
+        self._send(json_msg)
+    def _send(self, json_string):
+        data = {'session_json': json_string,
+                'User-Agent': 'bdbcontrib %s' % (__version__,)
+               }
+        t = threading.Thread(target=self._post,
+                             args=(self._url,),
+                             kwargs={'data': data})
+        t.start()
+        # I don't care if it finishes. We tried.
+
+import json
+def query_info_to_json(session_id, logtype, query, bindings,
+                       start_time, error, end_time):
+  session = {'entries': [[session_id, logtype, query + json.dumps(bindings),
+                          start_time, error, end_time]],
+             'fields': ['session_id', 'type', 'data',
+                        'start_time', 'error', 'end_time'],
+             'version': __version__,
+  }
+  return json.dumps(session)
+
+import traceback
+from contextlib import contextmanager
+@contextmanager
+def logged_query(query_string=None, bindings=(), name=None, logger=None):
+  if query_string is None:
+    query_string = ""
+  if logger is None:
+    logger = CallHomeStatusLogger()
+
+  if name is None:
+    yield  # Do no logging without a name to log by.
+  else:
+    start_time = time.time()
+    try:
+      yield
+      logger.info(query_info_to_json(
+          name, 'logged_query', query_string, bindings,
+          start_time, None, time.time()))
+    except:
+      logger.warn(query_info_to_json(
+          name, 'logged_query', query_string, bindings,
+          start_time, traceback.format_exc(), time.time()))
+      raise
