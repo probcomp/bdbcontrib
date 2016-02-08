01c3ee0e9e338d739214c3235f8205031da39795
Gregory Marton <gremio@acm.org>
2016-02-08 13:41:34 -0500

    Updated based on bayeslite's check.sh: is now __ci_ aware, and uses pytest as a library.

--- check.sh
+++ check.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-set -Ceu
-
 : ${PYTHON:=python}
-: ${PY_TEST:=`which py.test`}
 
-if [ ! -x "${PY_TEST}" ]; then
-    printf >&2 'unable to find pytest\n'
+lib_avail=`./pythenv.sh ${PYTHON} -m pytest --version 2>&1 | grep 'This is pytest'`
+if [ -z "$lib_avail" ]; then
+    printf >&2 'Unable to find pytest. Consider running python setup.py test.\n'
     exit 1
 fi
 
@@ -19,5 +17,14 @@ root=`cd -- "$(dirname -- "$0")" && pwd`
     "$PYTHON" setup.py build
     export BAYESDB_DISABLE_VERSION_CHECK=1
     export BAYESDB_WIZARD_MODE=1
-    ./pythenv.sh "$PYTHON" "$PY_TEST" "$@"
+    if [ $# -eq 0 ]; then
+        # By default, when running all tests, skip tests that have
+        # been marked for continuous integration by using __ci_ in
+        # their names.  (git grep __ci_ to find these.)
+        ./pythenv.sh "$PYTHON" -m pytest -k "not __ci_" tests
+    else
+        # If args are specified, run all tests, including continuous
+        # integration tests, for the selected components.
+        ./pythenv.sh "$PYTHON" -m pytest "$@"
+    fi
 )
