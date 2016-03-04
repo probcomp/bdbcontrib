8a32d781acb73a7133b877b0c56e00c3fc4cb17d
F Saad <fsaad@mit.edu>
2016-03-04 15:47:56 -0500

    Remove unecessary complicated plotter.

--- examples/medialab/mml.py
+++ examples/medialab/mml.py
@@ -4,6 +4,7 @@ import warnings
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 import bayeslite
 import bdbcontrib
@@ -122,11 +123,10 @@ def query_period_perigee_given_purpose(bdb, gpm):
             SIMULATE period_minutes, perigee_km FROM
             {gpm} GIVEN purpose = "Communications" LIMIT 100;'''
 
-    bdb.execute(tech_dev_bql.format(gpm=gpm))
-    bdb.execute(remote_sense_bql.format(gpm=gpm))
-    bdb.execute(comm_bql.format(gpm=gpm))
+    for bql in [tech_dev_bql, remote_sense_bql, comm_bql]:
+        bdb.execute(bql.format(gpm=gpm))
 
-def plot_period_perigee_given_purpose(bdb, create=False):
+def plot_period_perigee_given_purpose(bdb, gpm):
     "Plot Period, Perigee given Purpose."
     matplotlib.rcParams['legend.fontsize'] = 14
     matplotlib.rcParams['axes.titlesize'] = 18
@@ -137,21 +137,22 @@ def plot_period_perigee_given_purpose(bdb, create=False):
     matplotlib.rcParams['xtick.labelsize'] = 16
     matplotlib.rcParams['ytick.labelsize'] = 16
 
-    # else:
-    #     tech_dev = np.loadtxt('resources/TPgPurpose/tech_dev_cc')
-    #     remote_sense = np.loadtxt('resources/TPgPurpose/remote_sense_cc')
-    #     communications = np.loadtxt('resources/TPgPurpose/communications_cc')
-
-    # purpose_class_pairs = bdb.execute('''SELECT purpose, class_of_orbit,
-    #     count(*) AS count FROM satellites GROUP BY purpose, class_of_orbit
-    #     ORDER BY count DESC;''').fetchall()
+    tech_dev = np.asarray(
+        bdb.sql_execute(
+            '''SELECT * FROM tech_dev_{}'''.format(gpm)).fetchall())
+    remote_sense = np.asarray(
+        bdb.sql_execute(
+            '''SELECT * FROM remote_sense_{}'''.format(gpm)).fetchall())
+    comm = np.asarray(
+        bdb.sql_execute(
+            '''SELECT * FROM comm_{}'''.format(gpm)).fetchall())
 
     _, ax = plt.subplots()
     ax.scatter(tech_dev[:,1], tech_dev[:,0], color='blue',
         label='Technology Development [Primarily LEO]')
     ax.scatter(remote_sense[:,1], remote_sense[:,0], color='green',
         label='Remote Sensing [Primarily LEO]')
-    ax.scatter(communications[:,1], communications[:,0], color='red',
+    ax.scatter(comm[:,1], comm[:,0], color='red',
         label='Communications [Primarly GEO]')
 
     ax.set_title('SIMULATE Perigee_km, Period_minutes GIVEN Purpose LIMIT 100')
@@ -159,66 +160,11 @@ def plot_period_perigee_given_purpose(bdb, create=False):
     ax.set_ylabel('Period [Minutes]')
     ax.set_xlim([-200, 48000])
     ax.set_ylim([-20, 1700])
-    ax.legend(loc='upper left', framealpha=0, prop={'weight':'bold'})
     ax.grid()
+    ax.legend(loc='upper left', framealpha=0, prop={'weight':'bold'})
 
-def plot_T_given_CO(bdb, create=False):
-    "Plot Period given Class of Orbit."
-    if create:
-        leo = bdb.execute('''SELECT Period_minutes FROM satellites WHERE
-            Class_of_orbit = "LEO"''').fetchall()
-        geo = bdb.execute('''SELECT Period_minutes FROM satellites WHERE
-            Class_of_orbit = "GEO"''').fetchall()
-        meo = bdb.execute('''SELECT Period_minutes FROM satellites WHERE
-            Class_of_orbit = "MEO"''').fetchall()
-        elliptical = bdb.execute('''SELECT Period_minutes FROM satellites
-            WHERE Class_of_orbit = Elliptical''').fetchall()
-        samples_leo = bdb.execute('''SIMULATE Period_minutes FROM satcomp GIVEN
-            Class_of_orbit = "LEO" LIMIT 100;''').fetchall()
-        samples_geo = bdb.execute('''SIMULATE Period_minutes FROM satcomp GIVEN
-            Class_of_orbit = "GEO" LIMIT 100;''').fetchall()
-        samples_meo = bdb.execute('''SIMULATE Period_minutes FROM satcomp GIVEN
-            Class_of_orbit = "MEO" LIMIT 100;''').fetchall()
-        samples_elliptical = bdb.execute('''SIMULATE Period_minutes FROM satcomp
-            GIVEN Class_of_orbit = "Elliptical" LIMIT 100;''').fetchall()
-    else:
-        leo = np.loadtxt('resources/TgCO/leo')
-        geo = np.loadtxt('resources/TgCO/geo')
-        meo = np.loadtxt('resources/TgCO/meo')
-        elliptical = np.loadtxt('resources/TgCO/elliptical')
-        samples_leo = list(np.loadtxt('resources/TgCO/samples_leo'))
-        samples_geo = list(np.loadtxt('resources/TgCO/samples_geo'))
-        samples_meo = list(np.loadtxt('resources/TgCO/samples_meo'))
-        samples_elliptical = list(np.loadtxt('resources/TgCO/samples_elliptical'))
+def sklearn_analysis():
+    # Load the dataset.
+    dataset = pd.read_csv('resources/satellites.csv')
 
-    fig, ax = plt.subplots(2,1)
-
-    ax[0].hlines(leo, xmin=0, xmax=1, label='LEO', color='blue')
-    ax[0].hlines(geo, xmin=1, xmax=2, label='MEO', color='red')
-    ax[0].hlines(meo, xmin=2, xmax=3, label='GEO', color='green')
-    ax[0].hlines(elliptical, xmin=3, xmax=4, label='Elliptical', color='black')
-    ax[0].set_title('SELECT Period_minutes, Class_of_orbit FROM'
-        ' satellites ORDER BY Class_of_orbit''', fontweight='bold',size=16)
-
-    ax[1].hlines(samples_leo[:10]+samples_leo[-10:], xmin=0, xmax=1,
-        label='LEO', color='blue')
-    ax[1].hlines(samples_meo[:10]+samples_meo[-10:],
-        xmin=1, xmax=2, label='MEO', color='red')
-    ax[1].hlines(samples_geo[60:70] + samples_geo[80:90],
-        xmin=2, xmax=3, label='GEO', color='green')
-    ax[1].hlines(samples_elliptical[:10]+samples_elliptical[-10:],
-        xmin=3, xmax=4, label='Elliptical', color='black')
-    ax[1].set_title('''
-        SIMULATE Period_minutes FROM satellites GIVEN Class_of_orbit''',
-        fontweight='bold', size=16)
-
-    for a in ax:
-        a.set_xlim([0,4])
-        a.set_xlim([0,4])
-        a.set_ylim([0,4000])
-        a.set_ylim([0,4000])
-        a.set_xticks([0.5, 1.5, 2.5, 3.5])
-        a.set_xticklabels(['LEO','MEO','GEO','Elliptical'])
-        a.set_ylabel('Period (minutes)', fontweight='bold', size=16)
-        a.grid()
-        a.grid()
+bdb = load_bdb(filename)
