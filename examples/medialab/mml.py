9376dd5b5041748189d10866c9e8944dfd900577
F Saad <fsaad@mit.edu>
2016-03-07 18:59:07 -0500

    Update the MML scripter for medialab talk.

--- examples/medialab/mml.py
+++ examples/medialab/mml.py
@@ -1,13 +1,18 @@
-from datetime import datetime
+# -*- coding: utf-8 -*-
 import warnings
+from datetime import datetime
+from textwrap import wrap
 
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
 
 import bayeslite
 import bdbcontrib
 
+from bdbcontrib import query
 from bdbcontrib.metamodels.composer import Composer
 from bdbcontrib.predictors import keplers_law
 from bdbcontrib.predictors import random_forest
@@ -105,7 +110,7 @@ def initialize_analyse_satcc(bdb):
         );''')
 
     print 'Initializing'
-    bdb.execute('INITIALIZE 1 MODEL FOR satcc;')
+    bdb.execute('INITIALIZE 64 MODELS FOR satcc;')
     print 'Analyze'
     bdb.execute('ANALYZE satcc FOR 1500 ITERATION WAIT;')
 
@@ -124,6 +129,56 @@ def query_period_perigee_given_purpose(bdb, gpm):
 
     for bql in [tech_dev_bql, remote_sense_bql, comm_bql]:
         bdb.execute(bql.format(gpm=gpm))
+
+def query_dry_mass_mutual_information(bdb):
+    query(bdb, '''
+        CREATE TABLE dry_mass_mi AS ESTIMATE c.name,
+            MUTUAL INFORMATION WITH dry_mass_kg AS "MI(col:dry_mass) [linfoot]"
+        FROM COLUMNS OF satcc;''')
+
+def query_select_country_purpose_given_geo(bdb):
+    country_purpose_select = query(bdb, '''
+        CREATE TABLE IF NOT EXISTS country_purpose_select AS
+            SELECT country_of_operator || "--" || purpose AS "Country-Purpose",
+                COUNT("Country-Purpose") AS frequency
+        FROM satellites
+            WHERE Class_of_orbit = "GEO"
+            GROUP BY "Country-Purpose"
+            ORDER BY frequency DESC
+        LIMIT 20
+        ''')
+
+def query_simulate_country_purpose_given_geo(bdb):
+    query(bdb, '''
+        CREATE TABLE country_purpose_sim_raw AS
+            SIMULATE country_of_operator, purpose
+        FROM satcc GIVEN Class_of_orbit = 'GEO' LIMIT 1000;
+    ''');
+    query(bdb,'''
+        CREATE TABLE country_purpose_sim AS
+            SELECT country_of_operator || "--" || purpose AS "Country-Purpose",
+                COUNT("Country-Purpose") AS frequency
+            FROM country_purpose_sim_raw
+                GROUP BY "Country-Purpose"
+                ORDER BY frequency DESC
+            LIMIT 20;
+    ''')
+
+def query_simulate_country_purpose_given_geo_dm(bdb):
+    query(bdb, '''
+        CREATE TABLE country_purpose_dm_sim_raw AS
+            SIMULATE country_of_operator, purpose
+        FROM satcc GIVEN Class_of_orbit = "GEO", Dry_mass_kg = 500 LIMIT 1000;
+    ''');
+    query(bdb,'''
+        CREATE TABLE country_purpose_dm_sim AS
+            SELECT country_of_operator || "--" || purpose AS "Country-Purpose",
+                COUNT("Country-Purpose") AS frequency
+            FROM country_purpose_dm_sim_raw
+                GROUP BY "Country-Purpose"
+                ORDER BY frequency DESC
+            LIMIT 20;
+    ''')
 
 def plot_period_perigee_given_purpose(bdb, gpm):
     "Plot Period, Perigee given Purpose."
@@ -162,4 +217,104 @@ def plot_period_perigee_given_purpose(bdb, gpm):
     ax.grid()
     ax.legend(loc='upper left', framealpha=0, prop={'weight':'bold'})
 
+def plot_apogee_perigee_colorby_class(bdb):
+    return bdbcontrib.pairplot(bdb,
+        'SELECT apogee_km, perigee_km, class_of_orbit FROM satellites;',
+        colorby='class_of_orbit')
+
+def combine_dataframes(A, B):
+    A_zero_uniq = set(A.ix[:,0])
+    B_zero_uniq = set(B.ix[:,0])
+    A = A.set_index(A.columns[0])
+    B = B.set_index(B.columns[0])
+    C = pd.DataFrame(columns=['a','b'],
+        index=set.union(A_zero_uniq, B_zero_uniq))
+    for k in C.index:
+        a_val = A.loc[k].iloc[0] if k in A.index else 0
+        b_val = B.loc[k].iloc[0] if k in B.index else 0
+        C.loc[k] = [a_val, b_val]
+    return C.sort(columns=['b'], ascending=True).reset_index()
+
+def barplot(df, color='#333333', label=None):
+    if df.shape[1] != 2:
+        raise ValueError('Need two columns.')
+    figure, ax = plt.subplots()
+    ax.barh(range(df.shape[0]), df.ix[:,1].values,
+        color=color, edgecolor=color, alpha=.7, label=label)
+    ax.set_yticks(range(df.shape[0]))
+    ax.set_yticklabels(df.ix[:,0].values, fontweight='bold')
+    ax.set_ylabel(df.columns[0], fontweight='bold')
+    ax.set_xlabel(df.columns[1], fontweight='bold')
+    figure.set_tight_layout(True)
+    ax.legend(loc='lower right')
+    return figure
+
+def barplot_overlay(df, colors=['g','r']):
+    if df.shape[1] != 3:
+        raise ValueError('Need three columns.')
+    figure, ax = plt.subplots()
+    ind = range(df.shape[0])
+    width = .35
+    ax.barh(ind, df.ix[:,1].values, width,
+        color=colors[0], alpha=.7, label=df.columns[1])
+    ax.barh([y + width for y in ind], df.ix[:,2].values, width,
+        color=colors[1], alpha=.7, label=df.columns[2])
+    ax.set_yticks([y + width for y in ind])
+    ax.set_yticklabels(df.ix[:,0].values, fontweight='bold')
+    ax.set_ylabel('Country-Purpose', fontweight='bold')
+    ax.set_xlabel('Probability', fontweight='bold')
+    ax.legend(loc='lower right')
+    figure.set_tight_layout(True)
+    return figure
+
 bdb = load_bdb(filename)
+
+bdb.sql_execute('DROP TABLE IF EXISTS dry_mass_mi')
+bdb.sql_execute('DROP TABLE IF EXISTS country_purpose_select')
+bdb.sql_execute('DROP TABLE IF EXISTS country_purpose_sim_raw')
+bdb.sql_execute('DROP TABLE IF EXISTS country_purpose_sim')
+bdb.sql_execute('DROP TABLE IF EXISTS country_purpose_dm_sim_raw')
+bdb.sql_execute('DROP TABLE IF EXISTS country_purpose_dm_sim')
+
+query_dry_mass_mutual_information(bdb)
+query_select_country_purpose_given_geo(bdb)
+query_simulate_country_purpose_given_geo(bdb)
+query_simulate_country_purpose_given_geo_dm(bdb)
+
+# Compare country_purpose_select to country_purpose_simulate.
+cp_select = query(bdb, 'SELECT * FROM country_purpose_select;')
+cp_simulate = query(bdb, 'SELECT * FROM country_purpose_sim;')
+cp_simulate_dm = query(bdb, 'SELECT * FROM country_purpose_dm_sim;')
+
+# Normalize.
+cp_select['frequency'] /= 1167.
+cp_simulate['frequency'] /= 1000.
+cp_simulate_dm['frequency'] /= 1000.
+
+# Rename the columns.
+cp_select.columns = ['Country-Purpose', 'Probability']
+cp_simulate.columns = ['Country-Purpose', 'Probability']
+cp_simulate_dm.columns = ['Country-Purpose', 'Probability']
+
+# Histograms for raw data.
+barplot(cp_select.sort(columns=['Probability']), color='b',
+    label='SELECT country-purpose GIVEN geo')
+barplot(cp_simulate.sort(columns=['Probability']), color='r',
+    label='SIMULATE country-purpose GIVEN geo')
+barplot(cp_simulate_dm.sort(columns=['Probability']), color='g',
+    label='SIMULATE country-purpose GIVEN geo, dry_mass_kg = 500')
+
+# Histogram for raw vs simulated.
+joined_select_sim = combine_dataframes(cp_select, cp_simulate)
+joined_select_sim.columns = ['Country-Purpose',
+    'SELECT country-purpose GIVEN geo', 'SIMULATE country-purpose GIVEN geo']
+barplot_overlay(joined_select_sim, colors=['b','r'])
+
+# Histogram for raw vs simulated.
+joined_sim_dm = combine_dataframes(cp_simulate, cp_simulate_dm)
+joined_sim_dm.columns = ['Country-Purpose',
+    'SIMULATE country-purpose GIVEN geo',
+    'SIMULATE country-purpose GIVEN geo, dry_mass_kg = 500',]
+barplot_overlay(joined_sim_dm, colors=['r','g'])
+
+# plt.close('all')
