3996c82214e8d9016551040f6909182ea1331cf3
Gregory Marton <gremio@acm.org>
2016-04-07 17:59:36 -0400

    use pdf backend for satellites testing, too.

--- examples/tests/test_satellites.py
+++ examples/tests/test_satellites.py
@@ -19,6 +19,9 @@ import os
 import pytest
 import re
 
+import matplotlib
+matplotlib.use('pdf')
+
 from bdbcontrib.verify_notebook import run_and_verify_notebook
 from bdbcontrib.verify_notebook import assert_markdown_matches as md
 from bdbcontrib.verify_notebook import assert_markdown_not as md_not
